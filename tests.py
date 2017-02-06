0bdb5104e559933d4a94cbf2e0c7d12c9fe19506
Laurent Gautier <lgautier@gmail.com>
2017-02-05 21:18:57 -0500

    Fixed test after move of default entry building to tuple.

--- tests.py
+++ tests.py
@@ -7,8 +7,9 @@ def _test_readfastq_iter(filename, bufsize, entrypos):
     with open(filename, 'rb') as fh, open(filename, 'rt') as fh_bp:
         for entry, entry_bp in zip(fastqandfurious.readfastq_iter(fh, bufsize, _entrypos=entrypos),
                                    SeqIO.parse(fh_bp, "fastq")):
-            assert entry.header == b'@'+entry_bp.description.encode('ascii')
-            assert entry.sequence == str(entry_bp.seq).encode('ascii')
+            header, sequence, quality = entry
+            assert header == entry_bp.description.encode('ascii')
+            assert sequence == str(entry_bp.seq).encode('ascii')
 
 def test_readfastq_iter():
     for filename in ("data/test.fq", "data/test_longqualityheader.fq"):
