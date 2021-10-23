8b71e5a6a195c024dd012fa9f2574af1a236ab97
Laurent Gautier <lgautier@gmail.com>
2021-10-23 04:13:10 -0400

    Change Sphinx theme.

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'fastq-and-furious'
@@ -51,9 +50,11 @@ autodoc_typehints = 'description'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pyramid'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = ['my-styles.css']
