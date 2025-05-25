fd22951b56994a2fe3473ee3c037073f7f5ff58e
Laurent Gautier <lgautier@gmail.com>
2025-05-25 15:41:24 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, L. Gautier'
 author = 'L. Gautier'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.3'
+release = '0.3.4'
 
 
 # -- General configuration ---------------------------------------------------
